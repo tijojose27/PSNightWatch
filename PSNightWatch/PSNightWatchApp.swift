9279b0b09d32fe083489465ae4420da58f82390d
Tijo <tijojose27@yahoo.com>
2020-12-19 11:22:01 -0600

    Initial Commit

---
+++ PSNightWatch/PSNightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PSNightWatchApp.swift
+//  PSNightWatch
+//
+//  Created by Tijo Njaravelil on 12/19/20.
+//
+
+import SwiftUI
+
+@main
+struct PSNightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
